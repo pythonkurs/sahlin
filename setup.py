5dbb50816c45ed69de6f4238e7998b5c6de312f9
ksahlin <kr_sahlin@hotmail.com>
2013-01-17 16:58:14 +0100

    Added setup files to my folder in Python course 2013

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='sahlin',
+    version='0.1dev',
+    author='Kristoffer Sahlin',
+    author_email='kristoffer.sahlin@scilifelab.se',
+    url='example.com',
+    packages=['sahlin',],
+    license='GPLv3',
+    long_description=open('README.md').read(),
+)
