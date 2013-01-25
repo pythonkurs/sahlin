581923d099dbc4ba939e62c71b64e4f3137aa403
ksahlin <kr_sahlin@hotmail.com>
2013-01-25 12:46:52 +0100

    Adding updated homework for Lecture2

--- sahlin/Lecture2.py
+++ sahlin/Lecture2.py
@@ -5,17 +5,16 @@ Created on Jan 25, 2013
 '''
 
 
-def GetNYTrainData(xml_file):
+def NYTrainOutagesByRepairs():
+    xml_file = "http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml"
     import untangle
     doc = untangle.parse(xml_file) #read in the XML file
     outages = doc.NYCOutages.outage # fetch the outages
-    return(outages)
-
-def CalculateStatusRepair(outages):
     iRepairCount = 0
     iTotOutages = len(outages)
     for outage in outages:
         reason = outage.get_elements('reason')[0].cdata #get the reason from each outage
         if reason == 'REPAIR': iRepairCount += 1 
-    return( iRepairCount / float(iTotOutages) )
+    print 'Fraction of outages caused by repairs:', iRepairCount / float(iTotOutages)
+
 
--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -4,9 +4,8 @@ Created on Jan 24, 2013
 
 @author: ksahlin
 '''
-from sahlin.Lecture2 import GetNYTrainData,CalculateStatusRepair
-xml_file = "http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml"
-outages = GetNYTrainData(xml_file)
-fFractionOfRepairs = CalculateStatusRepair(outages)
-print 'Fraction of repairs:', fFractionOfRepairs
+from sahlin.Lecture2 import NYTrainOutagesByRepairs
+NYTrainOutagesByRepairs()
+
+
 
