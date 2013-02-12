a69ef4c4d85178d992c5a97584fe920fc026cd78
ksahlin <kr_sahlin@hotmail.com>
2013-02-12 14:50:07 +0100

    Revised version of session3 assignment using with statement and surname as property with setter.

--- sahlin/session3.py
+++ sahlin/session3.py
@@ -8,11 +8,14 @@ Code for session3 homework
 import os
 
 class repo_dir(object):
-    def __enter__(self,path):
-        os.chdir(path)
+    def __init__(self,target_path):
+        self.target_path = target_path
+        self.original_path = os.getcwd()
+    def __enter__(self):
+        os.chdir(self.target_path)
             
-    def __exit__(self, path, type, value, traceback):
-        os.chdir(path)
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.original_path)
 
 
 class CourseRepo(object):
@@ -24,11 +27,19 @@ class CourseRepo(object):
         '''
         Constructor
         '''
-        self.surname = surname
+        self.required = [".git","setup.py","README.md","scripts/getting_data.py","scripts/check_repo.py",surname + "/__init__.py",surname + "/session3.py"]
+
         
     @property
-    def required(self):
-        return [".git","setup.py","README.md","scripts/getting_data.py","scripts/check_repo.py", self.surname+"/__init__.py",self.surname+"/session3.py"]
+    def surname(self):
+        return self.surname
+    
+    @surname.setter
+    def surname(self,new_surname):
+        self.required[-2] = new_surname + "/__init__.py"
+        self.required[-1] = new_surname + "/session3.py"
+        
+        
     
     def check(self):
         for sPath in self.required:
@@ -47,4 +58,4 @@ class CourseRepo(object):
 #repo.surname = "b"
 #print(repo.required[-1])
 ## prints b/session3.py
-#print repo.check()
+#print repo.check(), repo.required
