9524583fb89d719693575b0b06dbb30b83b3bce2
ksahlin <kr_sahlin@hotmail.com>
2013-02-14 08:48:14 +0100

    Adding module session4 containing functions for creating a data frame and finding most frequent day and hour for committing.

--- sahlin/session4.py
+++ sahlin/session4.py
@@ -9,12 +9,13 @@ import getpass
 import dateutil
 from dateutil import parser
 from pandas import DataFrame,Series
-import pandas
+
+from collections import Counter
+import datetime
 
 pwd = getpass.getpass()
     
 def ReturnDataFrame(repo):
-    #print repo['name']
     commits = requests.get("https://api.github.com/repos/%s/%s/commits" %(org,repo['name']), auth=("ksahlin", pwd))
     repo_commits = commits.json
     message_list = []
@@ -25,21 +26,27 @@ def ReturnDataFrame(repo):
     s = Series(message_list, index=date_list,name=repo['name']) 
     return DataFrame(s)
 
-def MostFrequentCommitTime():
-    return
+def MostFrequentCommitTime(all_commits_df):
+    freq_table = Counter()
+    for row in all_commits_df.iterrows():
+        freq_table[ (datetime.datetime.weekday(row[0]),row[0].hour) ] += 1
+        print datetime.datetime.weekday(row[0]),row[0].hour
+    
+    day_dict = {0 : 'Monday', 1 : 'Tuesday', 2 : 'Wednesday', 3 : 'Thursday', 4 : 'Friday', 5 : 'Saturday', 6 : 'Sunday'}
+    
+    return( day_dict[freq_table.most_common(1)[0][0][0]], freq_table.most_common(1)[0][0][1] )
 
 
 
 if __name__ == '__main__':
     org = 'pythonkurs'
-    #get all repos in pythoncourse
-    repos = requests.get("https://api.github.com/orgs/%s/repos" % org, auth=("ksahlin", pwd))
+    repos = requests.get("https://api.github.com/orgs/%s/repos" % org, auth=("ksahlin", pwd)) #get all repos in pythoncourse
     repos_data = repos.json
     all_commits_df = DataFrame()
-    #counter =0
     for repo in repos_data:
-        #counter +=1
         all_commits_df = all_commits_df.append(ReturnDataFrame(repo))
-        #if counter >=5:
-        #    break
-    #print all_commits_df.sort().index
+
+    day,hour = MostFrequentCommitTime(all_commits_df)
+    print 'Most common commit date. Day:',day ,'Hour(24h):',hour 
+
+    
